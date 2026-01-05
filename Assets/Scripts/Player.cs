e0fdac3cd4a84808d9cebdcebe6fcec11d7002a9
plrtp <plrtp@mail.ru>
2026-01-05 16:51:54 +0300

    update: 	- передвижение персонажа от первого лица 	- цвет материала персонажа, никнейм персонажа и его цвет 	  синхронизированы для работы по сети 	- пробный текст и кнопка отправки пробного текста 	  синхронизированы для работы по сети

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,88 @@
 using Mirror;
 using UnityEngine;
+using TMPro;
 
-public class Player : NetworkBehaviour
+namespace QuickStart
 {
-    [SerializeField] float speed = 5f;
-
-    void Update()
+    public class PlayerScript : NetworkBehaviour
     {
-        if (!isOwned) return;
+        public TextMeshPro playerNameText;
+        public GameObject floatingInfo;
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        private Material _playerMaterialClone;
+        private SceneScript _sceneScript;
 
-        Vector3 movement = speed * Time.deltaTime * new Vector3(h, 0, v);
-        transform.Translate(movement);
+        [SyncVar(hook = nameof(OnNameChanged))]
+        public string playerName;
+
+        [SyncVar(hook = nameof(OnColorChanged))]
+        public Color playerColor = Color.white;
+
+        void OnNameChanged(string _Old, string _New)
+        {
+            playerNameText.text = _New;
+        }
+
+        void OnColorChanged(Color _Old, Color _New)
+        {
+            playerNameText.color = _New;
+            _playerMaterialClone = new Material(GetComponent<Renderer>().material)
+            {
+                color = _New
+            };
+
+            GetComponent<Renderer>().material = _playerMaterialClone;
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            _sceneScript.playerScript = this;
+
+            Camera.main.transform.SetParent(transform);
+            Camera.main.transform.localPosition = new Vector3(0, 0, 0);
+
+            floatingInfo.transform.localPosition = new Vector3(0, -0.3f, 0.6f);
+            floatingInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            string name = "Player" + Random.Range(100, 999);
+            Color color = new(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            CmdSetupPlayer(name, color);
+        }
+
+        [Command]
+        public void CmdSetupPlayer(string _name, Color _col)
+        {
+            playerName = _name;
+            playerColor = _col;
+
+            _sceneScript.statusText = $"{playerName} joined.";
+        }
+
+        [Command]
+        public void CmdSendPlayerMessage()
+        {
+            if (_sceneScript)
+                _sceneScript.statusText = $"{playerName} says hello {Random.Range(10, 99)}";
+        }
+
+        private void Awake()
+        {
+            _sceneScript = FindFirstObjectByType<SceneScript>();
+        }
+
+        private void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                floatingInfo.transform.LookAt(Camera.main.transform);
+                return;
+            }
+
+            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+            float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
+
+            transform.Rotate(0, moveX, 0);
+            transform.Translate(0, 0, moveZ);
+        }
     }
 }
---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,25 @@
+using Mirror;
+using TMPro;
+
+namespace QuickStart
+{
+    public class SceneScript : NetworkBehaviour
+    {
+        public TextMeshProUGUI canvasStatusText;
+        public PlayerScript playerScript;
+
+        [SyncVar(hook = nameof(OnStatusTextChanged))]
+        public string statusText;
+
+        void OnStatusTextChanged(string _Old, string _New)
+        {
+            canvasStatusText.text = statusText;
+        }
+
+        public void ButtonSendMessage()
+        {
+            if (playerScript != null)
+                playerScript.CmdSendPlayerMessage();
+        }
+    }
+}
